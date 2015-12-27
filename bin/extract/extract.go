54b9fe6a0ca68a00292535e5857f0acd2ecc3cb3
Thijs van Dijk <thijs@hooi.gr>
2015-12-27 13:00:38 +0100

    Concat audio files using ffmpeg

--- bin/extract/extract.go
+++ bin/extract/extract.go
@@ -5,12 +5,17 @@ import (
 	"flag"
 	"fmt"
 	"github.com/ametheus/speeldoos"
+	"io"
 	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 )
 
 var (
 	input_xml = flag.String("input_xml", "", "Input XML file")
-	input_zip = flag.String("input_zip", "", "Input ZIP file")
+	bitrate   = flag.String("bitrate", "64k", "Output audio bitrate (mp3)")
 )
 
 func init() {
@@ -23,23 +28,111 @@ func main() {
 		log.Fatal(err)
 	}
 
-	liz := len(*input_zip)
-	zf, err := zip.OpenReader(*input_zip)
+	zm := &ZipMap{}
 
 	for _, pf := range foo.Performances {
-		fmt.Printf("Files in '%s' (%s):\n", pf.Work.Title, pf.Work.Composer.Name)
+		title := "(no title)"
+		if len(pf.Work.Title) > 0 {
+			title = pf.Work.Title[0].Title
+		}
 
-		for i, fn := range pf.SourceFiles {
-			fmt.Printf("   %s\n", fn)
-			if fn[0:liz] != *input_zip {
-				log.Printf("File %d does not appear to be in this zip archive", i)
-			} else {
-				for j, zfp := range zf.File {
-					if zfp.Name == fn[liz+1:] {
-						fmt.Printf("     Found at position %d in zip archive!\n", j)
-					}
-				}
+		log.Printf("Now processing: %s - %s", pf.Work.Composer.Name, title)
+
+		if len(pf.SourceFiles) == 0 {
+			log.Printf("%s - %s has no source files!\n", pf.Work.Composer.Name, title)
+		}
+		outp := fmt.Sprintf("%s - %s.mp3", pf.Work.Composer.Name, title)
+
+		inp := ""
+		for i, _ := range pf.SourceFiles {
+			inp = fmt.Sprintf("%s|/proc/self/fd/%d", inp, i+3)
+		}
+		inp = fmt.Sprintf("concat:%s", inp[1:])
+
+		cmd := exec.Command("ffmpeg", "-y", "-i", inp, "-c:a", "libmp3lame", "-b:a", *bitrate, outp)
+		cmd.ExtraFiles = make([]*os.File, len(pf.SourceFiles))
+
+		pipes := make([]*os.File, len(pf.SourceFiles))
+		for i, _ := range pf.SourceFiles {
+			cmd.ExtraFiles[i], pipes[i], err = os.Pipe()
+			if err != nil {
+				log.Fatal(err)
 			}
 		}
+
+		cmd.Start()
+
+		for i, fn := range pf.SourceFiles {
+			f, err := zm.Get(fn)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			// Tee hee. The code below superimposes all movements on top of each other.
+			// go func() {
+			// 	defer f.Close()
+			// 	defer pipes[i].Close()
+			// 	io.Copy(pipes[i], f)
+			// }()
+			_, err = io.Copy(pipes[i], f)
+			if err != nil {
+				log.Fatal(err)
+			}
+			f.Close()
+			pipes[i].Close()
+		}
+
+		err = cmd.Wait()
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
+	log.Printf("Finished processing\n")
+}
+
+type ZipMap struct {
+	zips map[string]*zip.ReadCloser
+}
+
+func (z *ZipMap) Get(filename string) (io.ReadCloser, error) {
+	rv, _ := os.Open(os.DevNull)
+
+	if z.zips == nil {
+		z.zips = make(map[string]*zip.ReadCloser)
+	}
+
+	// FIXME: I'm of the opinion that this should work: elems := filepath.SplitList(filename)
+	elems := strings.Split(filename, "/")
+	for i, elem := range elems {
+		if len(elem) < 5 || elem[len(elem)-4:] != ".zip" {
+			continue
+		}
+		zipfile := filepath.Join(elems[0 : i+1]...)
+		read, ok := z.zips[zipfile]
+		if !ok {
+			log.Printf("Opening zip file %s...\n", zipfile)
+			var err error
+			read, err = zip.OpenReader(zipfile)
+			if err != nil {
+				log.Print(err)
+				read = nil
+			}
+			z.zips[zipfile] = read
+		}
+
+		if read == nil {
+			continue
+		}
+
+		localfile := filepath.Join(elems[i+1:]...)
+
+		for _, zfp := range read.File {
+			if zfp.Name == localfile {
+				return zfp.Open()
+			}
+		}
+
+		return rv, os.ErrNotExist
+	}
+	return rv, os.ErrNotExist
 }
