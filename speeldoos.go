8efe29237dac7fbe1e25454f3f1da3c4226397dc
Thijs van Dijk <github@inurbanus.nl>
2017-12-13 23:22:08 +0100

    Documentation spree

--- speeldoos.go
+++ speeldoos.go
@@ -7,13 +7,19 @@ import (
 	"os"
 )
 
+// A Composer represents a composer
 type Composer struct {
 	Name string
-	ID   string `xml:"id,attr,omitempty"`
+
+	// A composer's ID is the page ID for their Wikipedia page
+	ID string `xml:"id,attr,omitempty"`
 }
 
+// Opus numbers, together with the composer, ideally uniquely identify a work
 type OpusNumber struct {
-	Number    string `xml:",chardata"`
+	Number string `xml:",chardata"`
+
+	// Some composers use a different index (e.g. 'BWV', 'KV', 'Wq', etc.)
 	IndexName string `xml:",attr,omitempty"`
 }
 
@@ -33,12 +39,23 @@ type Title struct {
 	Language string `xml:",attr,omitempty"`
 }
 
+// A Work represents a musical composition.
 type Work struct {
-	Composer   Composer
-	Title      []Title
+	Composer Composer
+
+	// A work may have more than one title in different languages.
+	Title []Title
+
+	// The opus number(s) for this composition. There may be more than one index
+	// for identifying works by this particular composer. Or none at all. Or
+	// this work may just not appear on any of them.
 	OpusNumber []OpusNumber
-	Parts      []Part `xml:"Parts>Part,omitempty"`
-	Year       int
+
+	// The parts that comprise this work, if any.
+	Parts []Part `xml:"Parts>Part,omitempty"`
+
+	// The year this composition was completed.
+	Year int
 }
 
 type Part struct {
@@ -46,36 +63,67 @@ type Part struct {
 	Number string `xml:"number,attr,omitempty"`
 }
 
+// A Performer in one particular recording of a Work.
+// An 'artist', if you will.
 type Performer struct {
 	Name string `xml:",chardata"`
+
+	// The type of performer. Suggested values:
+	//   * "performer"
+	//   * "soloist"
+	//   * "orchestra"
+	//   * "ensemble"
+	//   * "conductor"
 	Role string `xml:"role,attr,omitempty"`
 }
 
+// A path to a file comprising the recording
 type SourceFile struct {
 	Filename string `xml:",chardata"`
-	Disc     int    `xml:"disc,attr,omitempty"`
+
+	// The disc number, if applicable.
+	Disc int `xml:"disc,attr,omitempty"`
 }
 
 func (s SourceFile) String() string {
 	return s.Filename
 }
 
+// One (recording of a) performance of a Work.
 type Performance struct {
-	Work        Work
-	Year        int          `xml:",omitempty"`
+	Work Work
+
+	// The year in which the performance took place
+	Year int `xml:",omitempty"`
+
 	Performers  []Performer  `xml:"Performers>Performer"`
 	SourceFiles []SourceFile `xml:"SourceFiles>File"`
 }
 
+// A Carrier can represent either a physical medium or a packaged download.
+// An "album," in layman's terms.
 type Carrier struct {
-	XMLName      xml.Name `xml:"https://www.inurbanus.nl/NS/speeldoos/1.0 Carrier"`
-	Name         string
-	ID           string        `xml:",omitempty"`
-	Hash         string        `xml:"hash,attr,omitempty"`
-	Source       string        `xml:"source,attr,omitempty"`
+	XMLName xml.Name `xml:"https://www.inurbanus.nl/NS/speeldoos/1.0 Carrier"`
+
+	Name string
+
+	// A catalog number, or some other unique identifier within the collection
+	ID string `xml:",omitempty"`
+
+	// The SHA256 hash of the corresponding .zip bundle for this carrier.
+	Hash string `xml:"hash,attr,omitempty"`
+
+	// An indication of how this carrier ended up in this library.
+	// Suggested values:
+	//   * "WEB" (digital download)
+	//   * "CD" (ripped cd)
+	Source string `xml:"source,attr,omitempty"`
+
+	// The performances on this carrier
 	Performances []Performance `xml:"Performances>Performance"`
 }
 
+// Read a serialized Carrier from a file
 func ImportCarrier(filename string) (*Carrier, error) {
 	ip, err := ioutil.ReadFile(filename)
 	if err != nil {
@@ -91,6 +139,7 @@ func ImportCarrier(filename string) (*Carrier, error) {
 	return foo, nil
 }
 
+// Serialize the carrier to disk
 func (c *Carrier) Write(filename string) error {
 	op, err := os.Create(filename)
 	if err != nil {
