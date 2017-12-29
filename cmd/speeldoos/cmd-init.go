6cd795ea4bdb873593bb26af0812733441689f3b
Thijs van Dijk <github@inurbanus.nl>
2017-12-30 00:19:16 +0100

    [init] Autodetect source files in the current folder

--- cmd/speeldoos/cmd-init.go
+++ cmd/speeldoos/cmd-init.go
@@ -3,11 +3,14 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
-	"github.com/thijzert/speeldoos"
 	"os"
 	"path"
+	"regexp"
+	"sort"
 	"strconv"
 	"strings"
+
+	"github.com/thijzert/speeldoos"
 )
 
 var defaultIndexNames = map[string]string{
@@ -19,10 +22,126 @@ var defaultIndexNames = map[string]string{
 	"Wolfgang Amadeus Mozart":   "K",
 }
 
+type detectedDisc struct {
+	path  string
+	files []detectedFile
+}
+
+type detectedFile struct {
+	path  string
+	base  string
+	track int
+}
+
+var number *regexp.Regexp = regexp.MustCompile("\\d+")
+
+func detectFiles(dirname, ext string) []detectedFile {
+	rv := make([]detectedFile, 0, 10)
+	dir, err := os.Open(dirname)
+	if err != nil {
+		return rv
+	}
+
+	names, err := dir.Readdirnames(-1)
+	if err != nil {
+		return rv
+	}
+
+	sort.Strings(names)
+
+	for _, name := range names {
+		if name == "" || name[0:1] == "." || len(name) <= len(ext) {
+			continue
+		}
+		if name[len(name)-len(ext):] != ext {
+			continue
+		}
+
+		fi, err := os.Stat(path.Join(dirname, name))
+		if err != nil {
+			continue
+		}
+		if fi.IsDir() || fi.Size() < 4 {
+			continue
+		}
+
+		track := 0
+		digmatch := number.FindStringIndex(name)
+		if digmatch != nil {
+			fmt.Sscan(name[digmatch[0]:digmatch[1]], &track)
+		}
+
+		rv = append(rv, detectedFile{
+			path:  path.Join(dirname, name),
+			base:  name,
+			track: track,
+		})
+	}
+
+	return rv
+}
+
+func detectAllFiles(dirname, ext string) map[int]detectedDisc {
+	rv := make(map[int]detectedDisc)
+
+	dsf := detectFiles(dirname, ext)
+	if len(dsf) > 0 {
+		rv[0] = detectedDisc{
+			path:  ".",
+			files: dsf,
+		}
+	}
+
+	dir, err := os.Open(dirname)
+	if err != nil {
+		return rv
+	}
+
+	fii, err := dir.Readdir(-1)
+	if err != nil {
+		return rv
+	}
+
+	for _, fi := range fii {
+		if !fi.IsDir() {
+			continue
+		}
+
+		name := fi.Name()
+		if name == "" || name[0:1] == "." {
+			continue
+		}
+
+		disc := 0
+		digmatch := number.FindStringIndex(name)
+		if digmatch == nil {
+			continue
+		}
+		fmt.Sscan(name[digmatch[0]:digmatch[1]], &disc)
+
+		if _, ok := rv[disc]; ok {
+			continue
+		}
+
+		fullpath := path.Join(dirname, name)
+		ssf := detectFiles(fullpath, ext)
+		if len(ssf) > 0 {
+			rv[disc] = detectedDisc{
+				path:  fullpath,
+				files: ssf,
+			}
+		}
+	}
+
+	return rv
+}
+
 func init_main(args []string) {
 	if len(args) == 0 {
 		croak(fmt.Errorf("Specify at least one number of parts"))
 	}
+
+	detectedSourceFiles := detectAllFiles(".", ".flac")
 
 	pfsize := make([]int, 0, len(args))
 	total_tracks := 0
@@ -58,6 +177,38 @@ func init_main(args []string) {
 
 		if d_total != total_tracks {
 			croak(fmt.Errorf("Total tracks on all cd's (%d) does not match total number of parts (%d).", d_total, total_tracks))
+		}
+	}
+
+	if len(discsize) > 1 {
+		if len(detectedSourceFiles) > 0 {
+			if len(detectedSourceFiles) != len(discsize) {
+				croak(fmt.Errorf("Have %d discs, but detected %d sets of source files.", len(discsize), len(detectedSourceFiles)))
+			}
+
+			for i, size := range discsize {
+				if dd, ok := detectedSourceFiles[i+1]; ok {
+					if len(dd.files) != size {
+						croak(fmt.Errorf("Disc %d: expected %d tracks, but detected %d source files.", i+1, size, len(dd.files)))
+					}
+				} else {
+					croak(fmt.Errorf("Source files for disc %d not autodetected", i+1))
+				}
+			}
+		}
+	} else {
+		if len(detectedSourceFiles) > 1 {
+			croak(fmt.Errorf("Have 1 disc, but detected %d sets of source files.", len(detectedSourceFiles)))
+		}
+
+		if dd, ok := detectedSourceFiles[1]; ok {
+			detectedSourceFiles[0] = dd
+			delete(detectedSourceFiles, 1)
+		}
+		if dd, ok := detectedSourceFiles[0]; ok {
+			if len(dd.files) != total_tracks {
+				croak(fmt.Errorf("Have %d parts, but detected %d source files.", total_tracks, len(dd.files)))
+			}
 		}
 	}
 
@@ -111,13 +262,22 @@ func init_main(args []string) {
 				pf.Work.Parts[j].Part = "2222"
 			}
 			if len(discsize) > 1 {
+				fn := path.Join(fmt.Sprintf(Config.Init.DiscFormat, disc_index+1), fmt.Sprintf(Config.Init.TrackFormat, track_counter))
+				if dd, ok := detectedSourceFiles[disc_index+1]; ok {
+					fn = dd.files[track_counter-1].path
+				}
+
 				pf.SourceFiles[j] = speeldoos.SourceFile{
-					Filename: path.Join(fmt.Sprintf(Config.Init.DiscFormat, disc_index+1), fmt.Sprintf(Config.Init.TrackFormat, track_counter)),
+					Filename: fn,
 					Disc:     disc_index + 1,
 				}
 			} else {
+				fn := fmt.Sprintf(Config.Init.TrackFormat, track_counter)
+				if dd, ok := detectedSourceFiles[0]; ok {
+					fn = dd.files[track_counter-1].path
+				}
 				pf.SourceFiles[j] = speeldoos.SourceFile{
-					Filename: fmt.Sprintf(Config.Init.TrackFormat, track_counter),
+					Filename: fn,
 				}
 			}
 			track_counter++
